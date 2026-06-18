9760a210f4dc473735561b3bf08252f4ee8b3b0a
Leandro Nini <drfiemost@users.noreply.github.com>
2026-06-18 20:34:12 +0200

    Clear filter state on model change (#27) (#29)

--- src/SID.cpp
+++ src/SID.cpp
@@ -287,6 +287,8 @@ void SID::setChipModel(ChipModel new_model)
         voice[i].wave()->setWaveformModels(wavetables);
         voice[i].wave()->setPulldownModels(pulldowntables);
     }
+
+    filter->restart();
 }
 
 void SID::setCombinedWaveforms(CombinedWaveforms new_cws)
