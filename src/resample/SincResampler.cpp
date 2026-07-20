bcafa270e23ef8021cb5a21531845cc621591b6b
Leandro Nini <drfiemost@users.sourceforge.net>
2026-07-20 11:21:03 +0200

    Help older compilers

--- src/resample/SincResampler.cpp
+++ src/resample/SincResampler.cpp
@@ -41,15 +41,6 @@
 #  include <numbers>
 #endif
 
-
-#if __cplusplus >= 202306L
-#  define CONSTEXPR_FUNC  constexpr
-#  define CONSTEXPR_VAR   constexpr
-#else
-#  define CONSTEXPR_FUNC
-#  define CONSTEXPR_VAR   const
-#endif
-
 namespace reSIDfp
 {
 
@@ -66,8 +57,7 @@ constexpr int BITS = 16;
  * @param x evaluate I0 at x
  * @return value of I0 at x.
  */
-CONSTEXPR_FUNC
-double I0(double x)
+constexpr double I0(double x)
 {
     double sum = 1.;
     double u = 1.;
@@ -201,17 +191,21 @@ SincResampler::SincResampler(
 #  endif
 #endif
 
-    // 16 bits -> -96dB stopband attenuation.
-    CONSTEXPR_VAR double A = -20. * std::log10(1.0 / (1 << BITS));
     // A fraction of the bandwidth is allocated to the transition band, which we double
     // because we design the filter to transition halfway at nyquist.
     const double dw = (1. - 2.*highestAccurateFrequency / samplingFrequency) * PI * 2.;
 
+    // 16 bits -> -96dB stopband attenuation.
+#if __cplusplus >= 202306L
+    constexpr double A = -20. * std::log10(1.0 / (1 << BITS));
+#else
+    constexpr double A = 96.3295986125;
+#endif
     // For calculation of beta and N see the reference for the kaiserord
     // function in the MATLAB Signal Processing Toolbox:
     // http://www.mathworks.com/help/signal/ref/kaiserord.html
-    CONSTEXPR_VAR double beta = 0.1102 * (A - 8.7);
-    CONSTEXPR_VAR double I0beta = I0(beta);
+    constexpr double beta = 0.1102 * (A - 8.7);
+    constexpr double I0beta = I0(beta);
     const double cyclesPerSampleD = clockFrequency / samplingFrequency;
     const double inv_cyclesPerSampleD = samplingFrequency / clockFrequency;
 
