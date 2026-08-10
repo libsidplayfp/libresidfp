ccfc47936dcb5f350c62f536b0832af9b22de490
Leandro Nini <drfiemost@users.sourceforge.net>
2026-08-10 16:18:34 +0200

    Docs

--- src/residfp/residfp.h
+++ src/residfp/residfp.h
@@ -238,7 +238,7 @@ public:
      * When enabled the filter cutoff is lower.
      *
      * @param enable true to enable old 2200pF caps used on ASSY 326298
-     *               false to use the standard 470pF caps.
+     *               false to use the standard 470pF caps. (default off)
      */
     void enableOld6581caps(bool enable);
 
@@ -285,7 +285,7 @@ public:
      * Set the DAC leakage level.
      * Affects the envelope and waveforms.
      *
-     * @param level the leakage level, between 0 (no leakage) and 1
+     * @param level the leakage level, between 0 (no leakage) and 1 (standard leakage) (default 1.0)
      *
      * @since 1.2
      */
@@ -295,7 +295,7 @@ public:
      * Set the 6581 wave offset.
      * Affects the volume of digi samples.
      *
-     * @param offset the waveform offset, between 0 (faint digis) and 1 (loud digis)
+     * @param offset the waveform offset, between 0 (average digis) and 1 (loud digis) (default 1.0)
      *
      * @since 1.2
      */
@@ -305,7 +305,7 @@ public:
      * Set the DC-Blocker resistance.
      * Affects the highpass cutoff frequency.
      *
-     * @param res the resistance value, between 0 (~1.6Hz) and 1 (~16Hz)
+     * @param res the resistance value, between 0 (10KOhm => ~1.6Hz) and 1 (1KOhm => ~16Hz) (default 0.0)
      *
      * @since 1.2
      */
